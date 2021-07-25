e0b3f9a24a819f40d652aa755f34aebfee46e9ed
silatcha <silatchayvan@gmail.com>
2021-07-25 18:09:25 +0200

    modif bd

--- hotels (1).sql
+++ hotels (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : sam. 10 juil. 2021 à 10:53
+-- Généré le : Dim 25 juil. 2021 à 16:08
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
 
@@ -42,37 +42,39 @@ CREATE TABLE IF NOT EXISTS `chambre` (
 --
 
 INSERT INTO `chambre` (`id_chambre`, `code_chambre`, `categorie`, `prix`, `status`) VALUES
-(1, 'A003', 'salle de reunion', 150000, 'libre'),
-(2, 'A005', 'salle de reunion', 35000, 'libre'),
-(102, 'A002', 'chambre standart', 27000, 'libre'),
-(109, 'A006', 'chambre standart', 27000, 'libre'),
-(201, 'A007', 'chambre standart', 27000, 'libre'),
-(202, 'A007', 'chambre standart', 27000, 'libre'),
-(204, 'A007', 'chambre standart', 27000, 'libre'),
-(205, 'A008', 'chambre standart', 27000, 'libre'),
-(206, 'A009', 'chambre standart', 27000, 'libre'),
-(207, 'A0010', 'chambre standart', 27000, 'libre'),
-(208, 'A0011', 'chambre standart', 27000, 'libre'),
-(209, 'A0012', 'chambre standart', 27000, 'libre'),
-(107, 'A0013', 'chambre standart', 27000, 'libre'),
-(105, 'A0014', 'chambre standart', 27000, 'libre'),
-(103, 'A0015', 'chambre standart', 27000, 'libre'),
-(101, 'A001', 'chambre confort', 35000, 'libre'),
-(104, 'A0018', 'chambre confort', 35000, 'libre'),
-(203, 'A0019', 'chambre confort', 35000, 'libre'),
-(210, 'A0020', 'chambre confort', 35000, 'libre'),
-(212, 'A0021', 'chambre confort', 35000, 'libre'),
-(214, 'A0022', 'chambre confort', 35000, 'libre'),
-(301, 'A0023', 'chambre confort', 35000, 'libre'),
-(307, 'A0024', 'chambre confort', 35000, 'libre'),
-(308, 'A0025', 'chambre confort', 35000, 'libre'),
-(303, 'A0026', 'chambre confort', 35000, 'libre'),
-(304, 'A0027', 'chambre confort', 35000, 'libre'),
-(305, 'A0028', 'chambre confort', 35000, 'libre'),
-(306, 'A0016', 'chambre confort', 35000, 'libre'),
-(302, 'A0029', 'suite', 60000, 'libre'),
-(309, 'A0030', 'suite', 60000, 'libre'),
-(310, 'A0031', 'suite', 60000, 'libre');
+(1, 'A001', 'climatise', 35000, 'occupé'),
+(2, 'A002', 'chambre standart', 27000, 'occupé'),
+(3, 'A003', 'salle de reunion', 16000, 'libre'),
+(4, 'A004', 'chambre double', 3000, 'occupé'),
+(5, 'A005', 'salle de reunion', 16000, 'libre'),
+(6, 'A006', 'chambre standart', 27000, 'occupé'),
+(7, 'A007', 'chambre standart', 27000, 'occupé'),
+(8, 'A007', 'chambre standart', 27000, 'occupé'),
+(9, 'A007', 'chambre standart', 27000, 'occupé'),
+(10, 'A008', 'chambre standart', 27000, 'occupé'),
+(11, 'A009', 'chambre standart', 27000, 'libre'),
+(12, 'A0010', 'chambre standart', 27000, 'libre'),
+(13, 'A0011', 'chambre standart', 27000, 'libre'),
+(14, 'A0012', 'chambre standart', 27000, 'libre'),
+(15, 'A0013', 'chambre standart', 27000, 'occupé'),
+(16, 'A0014', 'chambre standart', 27000, 'libre'),
+(17, 'A0015', 'chambre standart', 27000, 'occupé'),
+(18, 'A0016', 'chambre standart', 27000, 'libre'),
+(19, 'A0017', 'chambre standart', 27000, 'libre'),
+(20, 'A0018', 'climatise', 35000, 'libre'),
+(21, 'A0019', 'climatise', 35000, 'occupé'),
+(22, 'A0020', 'climatise', 35000, 'occupé'),
+(23, 'A0021', 'climatise', 35000, 'occupé'),
+(24, 'A0022', 'climatise', 35000, 'libre'),
+(25, 'A0023', 'climatise', 35000, 'libre'),
+(26, 'A0024', 'climatise', 35000, 'libre'),
+(27, 'A0025', 'climatise', 35000, 'libre'),
+(28, 'A0026', 'climatise', 35000, 'libre'),
+(29, 'A0027', 'climatise', 35000, 'libre'),
+(30, 'A0028', 'climatise', 35000, 'libre'),
+(31, 'A0029', 'appartement', 16000, 'libre'),
+(32, 'A0030', 'appartement', 16000, 'libre'),
+(33, 'A0031', 'appartement', 16000, 'libre');
 
 -- --------------------------------------------------------
 
@@ -87,18 +89,19 @@ CREATE TABLE IF NOT EXISTS `chambreclient` (
   `id_chambre` int(11) NOT NULL,
   `date` datetime NOT NULL,
   PRIMARY KEY (`idchambreClient`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `chambreclient`
 --
 
 INSERT INTO `chambreclient` (`idchambreClient`, `id_client`, `id_chambre`, `date`) VALUES
-(6, 9, 1, '2021-06-20 14:33:55'),
-(7, 10, 4, '2021-06-20 14:34:31'),
-(8, 11, 5, '2021-06-20 14:42:02'),
-(9, 12, 2, '2021-06-20 14:44:14'),
-(11, 12, 1, '2021-06-29 01:22:50');
+(27, 27, 20, '2021-07-13 09:30:31'),
+(28, 27, 21, '2021-07-13 09:30:31'),
+(29, 27, 7, '2021-07-13 09:30:54'),
+(30, 28, 8, '2021-07-13 14:04:14'),
+(31, 29, 1, '2021-07-13 14:40:19'),
+(32, 29, 1, '2021-07-13 14:40:19');
 
 -- --------------------------------------------------------
 
@@ -114,18 +117,18 @@ CREATE TABLE IF NOT EXISTS `client` (
   `tel` int(11) NOT NULL,
   `cni` int(11) NOT NULL,
   `date_ajout` datetime NOT NULL,
-  PRIMARY KEY (`id_client`)
-) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id_client`),
+  UNIQUE KEY `tel` (`tel`,`cni`)
+) ENGINE=MyISAM AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `client`
 --
 
 INSERT INTO `client` (`id_client`, `nom`, `prenom`, `tel`, `cni`, `date_ajout`) VALUES
-(11, 'Russelle', 'SILATCHA CHOMBONG', 699552886, 336618, '2021-06-20 14:42:02'),
-(10, 'Sila', 'Russelle', 698375118, 33661, '2021-06-20 14:34:31'),
-(9, 'CHOMBONG', 'Russelle', 698375118, 123456, '2021-06-20 14:33:55'),
-(12, 'simeu', 'Thibaut', 661860171, 5646, '2021-06-29 14:44:14');
+(27, 'CHOMBONG', 'Russelle', 698375118, 123456, '2021-07-13 09:30:31'),
+(28, 'simeu', 'Thibaut', 789456123, 3366995, '2021-07-13 14:04:14'),
+(29, 'totos', 'tatas', 456321789, 157963, '2021-07-13 14:40:19');
 
 -- --------------------------------------------------------
 
@@ -144,18 +147,18 @@ CREATE TABLE IF NOT EXISTS `commande` (
   `date_commande` datetime NOT NULL,
   `nombre` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_commande`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `commande`
 --
 
 INSERT INTO `commande` (`id_commande`, `nom_commande`, `lieu`, `montant`, `status`, `id_client`, `date_commande`, `nombre`) VALUES
-(1, 'whisky', 'Restaurant', 40000, 0, 10, '2021-06-20 01:01:18', 4),
-(2, 'poisson', 'chambre', 1000, 0, 11, '2021-06-20 11:10:35', 3),
-(3, 'habit', 'Linge', 3000, 0, 12, '2021-06-20 11:10:35', 3),
-(4, 'ampoule', 'Divers', 500, 0, 9, '2021-06-20 11:12:35', 1),
-(16, '', 'divers', 700, 0, 9, '2021-06-09 04:02:46', 2);
+(36, '', 'Restaurant', 700, 0, 28, '2021-07-13 14:04:44', 2),
+(37, '', 'Linge', 10000, 0, 28, '2021-07-13 14:04:59', 1),
+(38, '', 'Restaurant', 500, 0, 28, '2021-07-13 14:05:26', 1),
+(39, '', 'Restaurant', 700, 0, 27, '2021-07-13 14:08:01', 2),
+(40, '', 'Divers', 500, 0, 27, '2021-07-13 14:09:53', 2);
 
 -- --------------------------------------------------------
 
@@ -168,10 +171,18 @@ CREATE TABLE IF NOT EXISTS `entree` (
   `id_entree` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(45) NOT NULL,
   `prix` int(11) NOT NULL,
-  `nombre` int(11) NOT NULL,
-  `date` date NOT NULL,
+  `date` datetime NOT NULL,
   PRIMARY KEY (`id_entree`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `entree`
+--
+
+INSERT INTO `entree` (`id_entree`, `nom`, `prix`, `date`) VALUES
+(1, 'reglement', 500, '2021-07-25 18:01:23'),
+(2, 'reglement', 500, '2021-07-25 18:03:51'),
+(3, 'reglement', 500, '2021-07-25 18:05:13');
 
 -- --------------------------------------------------------
 
@@ -187,14 +198,16 @@ CREATE TABLE IF NOT EXISTS `facture` (
   `total` float NOT NULL,
   `id_client` int(11) NOT NULL,
   PRIMARY KEY (`id_facture`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `facture`
 --
 
 INSERT INTO `facture` (`id_facture`, `reporter`, `reglement`, `total`, `id_client`) VALUES
-(1, 2400, 4, 2398, 9);
+(21, 16200, 10000, 6200, 27),
+(22, 24700, 0, 24700, 28),
+(23, 0, 0, 0, 29);
 
 -- --------------------------------------------------------
 
@@ -228,9 +241,16 @@ CREATE TABLE IF NOT EXISTS `sortie` (
   `id_sortie` int(11) NOT NULL AUTO_INCREMENT,
   `libelle` varchar(45) NOT NULL,
   `prix` int(11) NOT NULL,
-  `date` date NOT NULL,
+  `date` datetime NOT NULL,
   PRIMARY KEY (`id_sortie`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `sortie`
+--
+
+INSERT INTO `sortie` (`id_sortie`, `libelle`, `prix`, `date`) VALUES
+(1, 'Bar', 500, '2021-07-25 18:05:47');
 
 -- --------------------------------------------------------
 
@@ -255,7 +275,7 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
 INSERT INTO `utilisateur` (`id_user`, `nom`, `password`, `tel`, `cni`, `email`) VALUES
 (1, 'russelle', 'cool', 698375118, 0, ''),
 (2, 'chritelle', 'cool', 658312548, 0, '');
-
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
