a4951fbeda955da519ce821c4122f7a94492404e
silatcha <silatchayvan@gmail.com>
2021-08-04 00:42:40 +0200

    enregistrement client

--- hotel.sql
+++ hotel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 03 août 2021 à 13:30
+-- Généré le : mar. 03 août 2021 à 22:41
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
 
@@ -60,7 +60,7 @@ INSERT INTO `chambre` (`id_chambre`, `code_chambre`, `categorie`, `prix`, `statu
 (205, 'A008', 'chambre standart', 27000, 'libre', '2021-08-03 11:52:05', '2021-08-03 11:52:05'),
 (206, 'A009', 'chambre standart', 27000, 'libre', '2021-08-03 11:52:05', '2021-08-03 11:52:05'),
 (207, 'A0010', 'chambre standart', 27000, 'libre', '2021-08-03 11:52:05', '2021-08-03 11:52:05'),
-(208, 'A0011', 'chambre standart', 27000, 'libre', '2021-08-03 11:52:05', '2021-08-03 11:52:05'),
+(208, 'A0011', 'chambre standart', 27000, 'libre', '2021-08-03 11:52:05', '2021-08-04 00:39:04'),
 (209, 'A0012', 'chambre standart', 27000, 'libre', '2021-08-03 11:52:05', '2021-08-03 11:52:05'),
 (210, 'A0020', 'chambre confort', 35000, 'libre', '2021-08-03 11:52:05', '2021-08-03 11:52:05'),
 (212, 'A0021', 'chambre confort', 35000, 'libre', '2021-08-03 11:52:05', '2021-08-03 11:52:05'),
@@ -71,10 +71,10 @@ INSERT INTO `chambre` (`id_chambre`, `code_chambre`, `categorie`, `prix`, `statu
 (304, 'A0027', 'chambre confort', 35000, 'libre', '2021-08-03 11:52:05', '2021-08-03 11:52:05'),
 (305, 'A0028', 'chambre confort', 35000, 'libre', '2021-08-03 11:52:05', '2021-08-03 11:52:05'),
 (306, 'A0016', 'chambre confort', 35000, 'libre', '2021-08-03 11:52:05', '2021-08-03 11:52:05'),
-(307, 'A0024', 'chambre confort', 35000, 'libre', '2021-08-03 11:52:05', '2021-08-03 11:52:05'),
-(308, 'A0025', 'chambre confort', 35000, 'libre', '2021-08-03 11:52:05', '2021-08-03 11:52:05'),
+(307, 'A0024', 'chambre confort', 35000, 'libre', '2021-08-03 11:52:05', '2021-08-04 00:39:04'),
+(308, 'A0025', 'chambre confort', 35000, 'libre', '2021-08-03 11:52:05', '2021-08-04 00:39:04'),
 (309, 'A0030', 'suite', 60000, 'libre', '2021-08-03 11:52:05', '2021-08-03 11:52:05'),
-(310, 'A0031', 'suite', 60000, 'libre', '2021-08-03 11:52:05', '2021-08-03 11:52:05');
+(310, 'A0031', 'suite', 60000, 'libre', '2021-08-03 11:52:05', '2021-08-04 00:39:04');
 
 -- --------------------------------------------------------
 
@@ -93,7 +93,7 @@ CREATE TABLE IF NOT EXISTS `chambreclient` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`idchambreClient`)
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=59 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `chambreclient`
@@ -129,7 +129,7 @@ CREATE TABLE IF NOT EXISTS `client` (
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id_client`),
   UNIQUE KEY `tel` (`tel`,`cni`)
-) ENGINE=MyISAM AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=55 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `client`
@@ -211,7 +211,6 @@ CREATE TABLE IF NOT EXISTS `facture` (
   `reglement` float NOT NULL,
   `total` float NOT NULL,
   `id_client` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id_facture`)
@@ -221,10 +220,10 @@ CREATE TABLE IF NOT EXISTS `facture` (
 -- Déchargement des données de la table `facture`
 --
 
-INSERT INTO `facture` (`id_facture`, `reporter`, `reglement`, `total`, `id_client`, `id_user`, `created_at`, `updated_at`) VALUES
-(21, 16200, 10000, 6200, 27, 1, '2021-08-03 11:52:06', '2021-08-03 11:52:06'),
-(22, 24700, 0, 24700, 28, 1, '2021-08-03 11:52:06', '2021-08-03 11:52:06'),
-(23, 0, 0, 0, 29, 1, '2021-08-03 11:52:06', '2021-08-03 11:52:06');
+INSERT INTO `facture` (`id_facture`, `reporter`, `reglement`, `total`, `id_client`, `created_at`, `updated_at`) VALUES
+(21, 16200, 10000, 6200, 27, '2021-08-03 11:52:06', '2021-08-03 11:52:06'),
+(22, 24700, 0, 24700, 28, '2021-08-03 11:52:06', '2021-08-03 11:52:06'),
+(23, 0, 0, 0, 29, '2021-08-03 11:52:06', '2021-08-03 11:52:06');
 
 -- --------------------------------------------------------
 
@@ -248,7 +247,34 @@ CREATE TABLE IF NOT EXISTS `infosclient` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id_infosclient`)
-) ENGINE=MyISAM AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=72 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `infosclient`
+--
+
+INSERT INTO `infosclient` (`id_infosclient`, `pays`, `nationalite`, `profession`, `destination`, `transport`, `nbpersonne`, `date_arrive`, `date_depart`, `date_ajout`, `id_client`, `created_at`, `updated_at`) VALUES
+(56, 'cameroun', 'camerounaise', 'enseignant', 'bafoussam', 'voiture', 1, '2021-08-12', '2021-08-21', '2021-08-03 22:48:33', 29, '2021-08-03 20:48:33', '2021-08-03 22:48:33'),
+(55, 'cameroun', 'camerounaise', 'enseignant', 'bafoussam', 'voiture', 1, '2021-08-26', '2021-08-21', '2021-08-03 22:44:59', 44, '2021-08-03 20:44:59', '2021-08-03 22:44:59'),
+(54, 'cameroun', 'camerounaise', 'enseignant', 'bafoussam', 'voiture', 1, '2021-08-21', '2021-08-21', '2021-08-03 22:40:02', 43, '2021-08-03 20:40:02', '2021-08-03 22:40:02'),
+(53, 'cameroun', 'camerounaise', 'enseignant', 'bafoussam', 'voiture', 1, '2021-08-19', '2021-08-29', '2021-08-03 22:37:03', 42, '2021-08-03 20:37:03', '2021-08-03 22:37:03'),
+(52, 'cameroun', 'camerounaise', 'enseignant', 'bafoussam', 'voiture', 1, '2021-08-11', '2021-08-19', '2021-08-03 22:35:24', 41, '2021-08-03 20:35:24', '2021-08-03 22:35:24'),
+(51, 'cameroun', 'camerounaise', 'enseignant', 'bafoussam', 'voiture', 1, '2021-08-21', '2021-08-29', '2021-08-03 22:32:11', 40, '2021-08-03 20:32:11', '2021-08-03 22:32:11'),
+(57, 'cameroun', 'camerounaise', 'enseignant', 'bafoussam', 'voiture', 1, '2021-09-04', '2021-08-28', '2021-08-03 22:53:20', 45, '2021-08-03 20:53:20', '2021-08-03 22:53:20'),
+(58, 'cameroun', 'camerounaise', 'enseignant', 'bafoussam', 'voiture', 1, '2021-08-22', '2021-08-28', '2021-08-03 22:58:21', 45, '2021-08-03 20:58:21', '2021-08-03 22:58:21'),
+(59, 'cameroun', 'camerounaise', 'enseignant', 'bafoussam', 'voiture', 2, '2021-08-21', '2021-08-27', '2021-08-03 23:00:22', 46, '2021-08-03 21:00:22', '2021-08-03 23:00:22'),
+(60, 'cameroun', 'camerounaise', 'enseignant', 'bafoussam', 'voiture', 1, '2021-08-20', '2021-09-03', '2021-08-03 23:05:12', 47, '2021-08-03 21:05:12', '2021-08-03 23:05:12'),
+(61, 'cameroun', 'camerounaise', 'enseignant', 'bafoussam', 'voiture', 1, '2021-08-20', '2021-08-27', '2021-08-03 23:06:48', 48, '2021-08-03 21:06:48', '2021-08-03 23:06:48'),
+(62, 'cameroun', 'camerounaise', 'enseignant', 'bafoussam', 'voiture', 45, '2021-08-21', '2021-09-05', '2021-08-03 23:12:24', 49, '2021-08-03 21:12:24', '2021-08-03 23:12:24'),
+(63, 'cameroun', 'camerounaise', 'enseignant', 'bafoussam', 'voiture', 2, '2021-08-18', '2021-08-28', '2021-08-03 23:15:44', 50, '2021-08-03 21:15:44', '2021-08-03 23:15:44'),
+(64, 'cameroun', 'camerounaise', 'enseignant', 'bafoussam', 'voiture', 1, '2021-08-20', '2021-08-28', '2021-08-03 23:33:33', 51, '2021-08-03 21:33:33', '2021-08-03 23:33:33'),
+(65, 'cameroun', 'camerounaise', 'enseignant', 'bafoussam', 'voiture', 1, '2021-08-19', '2021-08-20', '2021-08-03 23:42:05', 51, '2021-08-03 21:42:05', '2021-08-03 23:42:05'),
+(66, 'cameroun', 'camerounaise', 'enseignant', 'bafoussam', 'voiture', 1, '2021-08-13', '2021-08-21', '2021-08-03 00:06:38', 51, '2021-08-03 22:06:38', '2021-08-04 00:06:38'),
+(67, 'cameroun', 'camerounaise', 'enseignant', 'bafoussam', 'voiture', 1, '2021-08-13', '2021-08-21', '2021-08-03 00:07:40', 51, '2021-08-03 22:07:40', '2021-08-04 00:07:40'),
+(68, 'cameroun', 'camerounaise', 'enseignant', 'bafoussam', 'voiture', 1, '2021-08-28', '2021-09-04', '2021-08-03 00:16:17', 52, '2021-08-03 22:16:17', '2021-08-04 00:16:17'),
+(69, 'cameroun', 'camerounaise', 'enseignant', 'bafoussam', 'voiture', 1, '2021-08-28', '2021-08-22', '2021-08-03 00:17:24', 53, '2021-08-03 22:17:24', '2021-08-04 00:17:24'),
+(70, 'cameroun', 'camerounaise', 'enseignant', 'bafoussam', 'voiture', 1, '2021-08-28', '2021-08-21', '2021-08-03 00:23:23', 53, '2021-08-03 22:23:23', '2021-08-04 00:23:23'),
+(71, 'cameroun', 'camerounaise', 'enseignant', 'bafoussam', 'voiture', 1, '2021-08-28', '2021-08-27', '2021-08-03 00:25:52', 54, '2021-08-03 22:25:52', '2021-08-04 00:25:52');
 
 -- --------------------------------------------------------
 
@@ -258,7 +284,7 @@ CREATE TABLE IF NOT EXISTS `infosclient` (
 
 DROP TABLE IF EXISTS `log`;
 CREATE TABLE IF NOT EXISTS `log` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_user` int(11) NOT NULL,
   `action` varchar(100) COLLATE utf16_german2_ci NOT NULL,
   `id_client` int(11) DEFAULT NULL,
@@ -267,8 +293,9 @@ CREATE TABLE IF NOT EXISTS `log` (
   `id_entree` int(11) DEFAULT NULL,
   `id_infos` int(11) DEFAULT NULL,
   `id_chambre` int(11) DEFAULT NULL,
-  `created_at` datetime NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf16 COLLATE=utf16_german2_ci;
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=93 DEFAULT CHARSET=utf16 COLLATE=utf16_german2_ci;
 
 -- --------------------------------------------------------
 
